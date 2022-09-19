9719d10fed7c593d8c9f3c41995794b08fc92aa0
CheMargo <112963238+CheMargo@users.noreply.github.com>
2022-09-19 17:57:06 +0600

    Update Program.cs

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -5,19 +5,6 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
-// Console.WriteLine("Введите первое число");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число");
-// int b = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("\nПервое число: " + a + " Второе число:" + b + "\n");
-// int max;
-// if (a>b){
-//     max = a;
-// }
-// else{
-//     max = b;
-// }
-// Console.WriteLine("Наибольшее число " + max);
 
 Console.WriteLine("Введите первое число");
 int a = Convert.ToInt32(Console.ReadLine());
